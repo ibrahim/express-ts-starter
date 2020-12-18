594252b3ae4cca3f2846b793db98d61ceffb6ffe
Ibrahim Ahmed <ia.ibrahim@gmail.com>
2020-12-19 01:32:40 +0200

    fix prettier and eslint configuration. Add lint-staged.

--- src/routes.ts
+++ src/routes.ts
@@ -3,13 +3,13 @@ import { PingController } from "./controllers/PingController";
 import { IndexController } from "./controllers/IndexController";
 
 const _routes: [string, Router][] = [
-	["/", IndexController],
-	["/ping", PingController],
+  ["/", IndexController],
+  ["/ping", PingController],
 ];
 
 export const routes = (app: Application) => {
-	_routes.forEach((route) => {
-		const [url, controller] = route;
-		app.use(url, controller);
-	});
+  _routes.forEach((route) => {
+    const [url, controller] = route;
+    app.use(url, controller);
+  });
 };
